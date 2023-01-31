f24b9acdbca0b5f921cfb566a6e4a2c5e6f7c0a3
5wanpyae <swanpyae823@gmail.com>
2023-01-31 12:38:28 +0700

    classcard

--- front-end/src/components/PotatoClassCard.svelte
+++ front-end/src/components/PotatoClassCard.svelte
@@ -1,1 +1,30 @@
-Hello
+<div class=" rounded-lg w-[300px] bg-[#0c1d34] shadow-md">
+    <img class=" rounded-t-lg" src="images/demo3.jpg" alt="Photo"/>
+    <div class="text-white flex-grow-0 p-3 space-y-3">
+        <div class="flex flex-row space-x-3 items-center">
+            <p class="text-2xl font-bold">ILE - 125</p>
+            <p class="text-sm text-gray-300">(Prof. Tim)</p>
+        </div>
+        
+        <div class="flex flex-row">
+            <div class="flex-2">
+                <p class="text-start">Room</p>
+                <p class="text-start">Room</p>
+                <p class="text-start">Room</p>
+            </div>
+            <div class="flex-1">
+                <div class="text-center">:</div>
+                <div class="text-center">:</div>
+                <div class="text-center">:</div>
+            </div>
+            <div class="flex-1">
+                <div class="text-start">7-101</div>
+                <div class="text-start">7-101</div>
+                <div class="text-start">7-101</div>
+            </div>
+            <div class="flex-grow"></div>
+        </div>
+        <button class="w-full h-14 bg-blue-600 rounded-md">Who With Me?</button>
+    </div>
+</div>
+	
